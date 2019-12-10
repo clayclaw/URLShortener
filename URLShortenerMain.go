57868587f074fcc11f3cd183e0cc2913d9cb79fd
s091424 <ihatehomework2014@gmail.com>
2019-12-10 23:05:44 +0800

    final test

--- URLShortenerMain.go
+++ URLShortenerMain.go
@@ -1,22 +1,26 @@
 package main
 
 import (
-	"clayclaw.me/urlshortener/services"
 	"fmt"
 	"log"
 	"net/http"
+
+	"clayclaw.me/urlshortener/services"
 )
 
 const port = "8080"
 
 func main() {
 
+	fmt.Printf("GCI, made with love <3\n")
 	services.RegisterURL("boss", "https://www.jboss.org/")
 	services.RegisterURL("google", "https://www.google.com/")
 	services.RegisterURL("duckduckgo", "https://duckduckgo.com/")
-	services.RegisterURL("illegalsite", "https://imillegalxxxxxx.net/")
+	services.RegisterURL("illegalsite", "https://thisisILLEGAL.youhaveneverseenbefore/")
 
 	services.RegisterBanURL("illegalsite")
+	// fmt.Printf("TEST1: %t", services.IsURLRegistered("illegalsite"))
+	// fmt.Printf("TEST2: %t", services.IsBanned("illegalsite"))
 
 	http.HandleFunc("/", redirect)
 
@@ -25,7 +29,7 @@ func main() {
 
 }
 
-func redirect(w http.ResponseWriter, r * http.Request){
+func redirect(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("\nFOUND: " + r.URL.Path[1:])
 
@@ -33,12 +37,13 @@ func redirect(w http.ResponseWriter, r * http.Request){
 
 		if !services.IsBanned(r.URL.Path[1:]) {
 			http.Redirect(w, r, services.GetURL(r.URL.Path[1:]), 301)
-		}else{
+		} else {
 			http.Redirect(w, r, "https://imgur.com/HNWaaAr", 301)
 		}
 
 	} else {
 		http.Redirect(w, r, "https://imgur.com/qTpzOFU", 301)
 	}
+	
 
 }
